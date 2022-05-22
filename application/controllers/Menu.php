a5736cc25272899d459dc71c3f55860d34cdf09a
Muhmammadsyarifrahman <106023133+Muhmammadsyarifrahman@users.noreply.github.com>
2022-05-22 23:13:12 +0800

    Add files via upload

---
+++ application/controllers/Menu.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Menu extends CI_Controller {
+	public function index()
+	{
+        $data = array(
+			'title' => 'Dashboard',
+            'content' => 'dashboard/index.php'
+        );
+		$this->load->view('admin/template/main',$data);
+	}
+}
